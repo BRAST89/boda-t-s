1186437868fe092a05cca49633d156f9f0f0b9d9
Steven Duran <100061375+BRAST89@users.noreply.github.com>
2024-11-03 20:44:50 -0500

    actual

--- scripts-tarjeta.js
+++ scripts-tarjeta.js
@@ -1,18 +1,21 @@
-// Seleccionar todas las secciones (slides)
-const slides = document.querySelectorAll('.slide');
-let currentSlide = 0;
+// Seleccionar la imagen de fondo
+const imagenFondo = document.querySelector('.imagen-fondo');
 
-// Función para cambiar las secciones
-function changeSlide() {
-  // Eliminar la clase 'active' de todas las secciones
-  slides.forEach(slide => slide.classList.remove('active'));
+// Mostrar la imagen de fondo inicial
+imagenFondo.style.backgroundImage = `url('images/fotos/foto-marco-desktop.png')`; // Imagen de escritorio por defecto
 
-  // Agregar la clase 'active' a la sección actual
-  slides[currentSlide].classList.add('active');
-
-  // Cambiar al siguiente slide o reiniciar
-  currentSlide = (currentSlide + 1) % slides.length;
+// Cambiar la imagen según el tamaño de la pantalla
+function ajustarImagenFondo() {
+    if (window.innerWidth >= 1024) {
+        imagenFondo.style.backgroundImage = `url('images/fotos/foto-marco-desktop.png')`;
+    } else {
+        imagenFondo.style.backgroundImage = `url('images/fotos/foto-marco-mobile-large.png')`;
+    }
 }
 
-// Iniciar el cambio de secciones cada 3 segundos
-setInterval(changeSlide, 3000);
+// Llamar a la función al cargar y redimensionar la ventana
+window.onload = ajustarImagenFondo;
+window.onresize = ajustarImagenFondo;
+
+
+
